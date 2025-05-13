fc48a26462c25351f695cfcdfef1e466b5ce0d5e
ekp529 <ekpaupulaire@gmail.com>
2025-05-12 21:02:38 -0400

    added image assets

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,7 +3,38 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+        img: "./assets/projector.jpg",
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "./assets/speakers.jpg",
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: "./assets/microphone.jpg",
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "./assets/whiteboard.png",
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
+
+    {
+        img: "./assets/signs.jpg",
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
+
   ],
 
 
